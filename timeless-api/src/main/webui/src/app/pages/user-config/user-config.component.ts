697786e4a76548456fef908fbcf5ad89574d2cc1
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-05-28 18:25:02 -0300

    Use reactive form on user config (#23)

--- timeless-api/src/main/webui/src/app/pages/user-config/user-config.component.ts
+++ timeless-api/src/main/webui/src/app/pages/user-config/user-config.component.ts
@@ -3,7 +3,7 @@ import {Card} from 'primeng/card';
 import {InputText} from 'primeng/inputtext';
 import {Button} from 'primeng/button';
 import {InputMask} from 'primeng/inputmask';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TimelessApiService} from '../../timeless-api.service';
 import {MessageService} from 'primeng/api';
 
@@ -14,32 +14,46 @@ import {MessageService} from 'primeng/api';
     InputText,
     Button,
     InputMask,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './user-config.component.html',
   styleUrl: './user-config.component.scss'
 })
 export class UserConfigComponent {
 
-  userInfo = signal<UserInfoResponse>({
-    id: '',
-    email: '',
-    firstName: '',
-    phoneNumber: '',
-    lastName: '',
-    hasPhoneNumber: true
+  formBuilder = inject(FormBuilder)
+
+  hasPhoneNumber = signal<boolean>(true)
+  form = this.formBuilder.group({
+    email: new FormControl({value: '', disabled: true}),
+    firstName: new FormControl({value: '', disabled: true}),
+    phoneNumber: [''],
+    lastName: new FormControl({value: '', disabled: true})
   })
 
   constructor(private readonly timelessApiService: TimelessApiService, private readonly messageService: MessageService) {
     this.timelessApiService.userInfo()
       .subscribe((response: any) => {
-        this.userInfo.update(value => ({...response}))
+        this.hasPhoneNumber.set(response.hasPhoneNumber)
+        this.form.patchValue({
+          email: response.email,
+          firstName: response.firstName,
+          lastName: response.lastName,
+          phoneNumber: response.phoneNumber
+        })
       })
   }
 
 
   update() {
-    this.timelessApiService.updatePhoneNumber(this.userInfo().phoneNumber)
+    const phoneNumber = this.form.value.phoneNumber;
+
+    if (!phoneNumber) {
+      return
+    }
+
+    this.timelessApiService.updatePhoneNumber(phoneNumber)
       .subscribe(_ => {
         this.messageService.add({
           key: "toast",
