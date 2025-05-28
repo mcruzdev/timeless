d5c6f0c84a2e14d5e121c44d903acc7ef0ac3cef
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-05-28 19:01:27 -0300

    Refactor

--- timeless-api/src/main/java/dev/matheuscruz/infra/ai/data/AiImageResponse.java
+++ timeless-api/src/main/java/dev/matheuscruz/infra/ai/data/AiImageResponse.java
@@ -1,7 +1,6 @@
 package dev.matheuscruz.infra.ai.data;
 
 import dev.matheuscruz.domain.RecordType;
-
 import java.math.BigDecimal;
 
 public record AiImageResponse(BigDecimal amount, String description, RecordType type, boolean withError) {
--- timeless-api/src/main/java/dev/matheuscruz/presentation/MessageResource.java
+++ timeless-api/src/main/java/dev/matheuscruz/presentation/MessageResource.java
@@ -37,7 +37,7 @@ public class MessageResource {
     private final ObjectMapper objectMapper;
 
     public MessageResource(TimelessAiService aiService, TimelessImageAiService imageAiService,
-                           RecordRepository recordRepository, ObjectMapper mapper, UserRepository userRepository) {
+            RecordRepository recordRepository, ObjectMapper mapper, UserRepository userRepository) {
         this.aiService = aiService;
         this.imageAiService = imageAiService;
         this.recordRepository = recordRepository;
@@ -58,7 +58,7 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response image(@Valid ImageRequest req) {
-        Log.info("Receiving message from WhatsApp bot" + req.from());
+
         User user = userRepository.findByPhoneNumber(req.from()).orElseThrow(NotFoundException::new);
         AiImageResponse imageResponse = imageAiService.handleTransactionImage(
                 Image.builder().base64Data(req.base64()).mimeType(req.mimeType()).build(), req.text());
@@ -68,13 +68,11 @@ public class MessageResource {
         }
 
         QuarkusTransaction.requiringNew().run(() -> {
-            this.recordRepository.persist(Record.create(
-                    user.getId(), imageResponse.amount(), imageResponse.description(), imageResponse.type()
-            ));
+            this.recordRepository.persist(Record.create(user.getId(), imageResponse.amount(),
+                    imageResponse.description(), imageResponse.type()));
         });
 
-        return Response.status(Response.Status.CREATED)
-                .entity(imageResponse).build();
+        return Response.status(Response.Status.CREATED).entity(imageResponse).build();
     }
 
     private Response handleMessage(User user, String message) {
