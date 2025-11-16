d52f718d11a45d2c8216fc2f3e4d2b522879cdd1
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-11-16 00:17:21 -0300

    Update AESAdapter

--- timeless-api/src/main/java/dev/matheuscruz/infra/security/AESAdapter.java
+++ timeless-api/src/main/java/dev/matheuscruz/infra/security/AESAdapter.java
@@ -14,26 +14,24 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @ApplicationScoped
 public class AESAdapter {
 
-    final String secret;
+    final SecretKeySpec key;
 
     public AESAdapter(@ConfigProperty(name = "security.sensible.secret") String secret) {
-        this.secret = secret;
+        this.key = new SecretKeySpec(Base64.getDecoder().decode(secret), "AES");
     }
 
     public String encrypt(String data) throws NoSuchPaddingException, NoSuchAlgorithmException,
             IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        SecretKeySpec key = new SecretKeySpec(this.secret.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, this.key);
         byte[] encrypted = cipher.doFinal(data.getBytes());
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
     public String decrypt(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException,
             IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        SecretKeySpec key = new SecretKeySpec(this.secret.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, this.key);
         byte[] original = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
         return new String(original);
     }
