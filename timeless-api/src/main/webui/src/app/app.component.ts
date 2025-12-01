baee94454be2e7074d7a8a5460f84d039de0ba79
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-12-01 18:04:27 -0300

    feat(sign-in): add feedback on sign-in page
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- timeless-api/src/main/webui/src/app/app.component.ts
+++ timeless-api/src/main/webui/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MessageService],
