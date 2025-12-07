271bcbe8b0a08b35d09392cc1ed1a2ea1efcaf48
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-12-07 00:25:52 -0300

    fix: allow only get my own balance
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- timeless-api/src/main/java/dev/matheuscruz/domain/RecordRepository.java
+++ timeless-api/src/main/java/dev/matheuscruz/domain/RecordRepository.java
@@ -1,6 +1,7 @@
 package dev.matheuscruz.domain;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.logging.Log;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
@@ -9,11 +10,9 @@ import java.util.List;
 public class RecordRepository implements PanacheRepository<Record> {
 
     public List<AmountAndTypeOnly> getRecordsWithAmountAndTypeOnlyByUser(String userId) {
+        Log.info("Getting balance for user ID: " + userId);
         return find("select r.amount, r.transaction from Record as r where userId = :userId",
                 Parameters.with("userId", userId)).project(AmountAndTypeOnly.class).list();
     }
 
-    public List<AmountAndTypeOnly> getRecordsWithAmountAndTypeOnly() {
-        return find("select r.amount, r.transaction from Record as r").project(AmountAndTypeOnly.class).list();
-    }
 }
